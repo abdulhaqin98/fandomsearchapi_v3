a6789d8c71118b2125c1637e3b608ab3f9665241
Apple <apple@Apples-MacBook-Pro.local>
2021-06-16 00:40:20 +0530

    v2

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const url = ['https://friends.fandom.com/',
 
 const series = ['Friends', 'H.I.M.Y.M', 'The Office (US)', 'The Big Bang Theory',
   'Harry Potter', 'Breaking Bad', 'PIXAR', 'G.O.T', 'Lord of the Rings',
-  'The Godfather','PotC','Sherlock','Dragonball','The Hunger Games'
+  'The Godfather','PotC','Sherlock','Dragonball','The Hunger Games',
   'M.C.U', 'Batman', 'D.C. Database', 'D.C Movies', 'D.C. Extended Universe'];
 
 var title;
